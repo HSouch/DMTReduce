47042dae8737abcdf7b00753f013454b95ce5847
harrison <hsouchereau@outlook.com>
2022-02-22 15:01:20 -0500

    Added setup, init, and main pipe files

--- dmtreduce/__init__.py
+++ dmtreduce/__init__.py
@@ -3,3 +3,5 @@ from cleaning import *
 from edges import *
 from preprocess import *
 from utils import *
+
+__version__ = "0.0.1"
--- setup.py
+++ setup.py
@@ -1,1 +1,29 @@
 # DMTReduce Pipeline
+
+from distutils.core import setup
+from setuptools import find_packages
+import dmtreduce
+
+setup(
+    name='tbridge',
+    packages=find_packages(),
+    version=dmtreduce.__version__,
+    license='MIT License',
+    description = 'Spectra reduction pipeline for LRIS broad slit and DMT.',
+    author="Harrison Souchereau",
+    author_email='harrison.souchereau@yale.edu',
+    keywords='',
+    scripts=[],
+    install_requires=[
+
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+
+    ],
+)
